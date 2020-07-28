7537b5ae614677aaf625d4e71c8cba3b9a4ec1fd
pavithra <68429671+pavithra2002@users.noreply.github.com>
2020-07-28 18:53:19 +0530

    Add files via upload

---
+++ MyProject2.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package my.project.pkg2;
+
+/**
+ *
+ * @author PAVITHRA T
+ */
+public class MyProject2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // addition of two numbers
+        int a=10;
+        int b=20;
+        int c;
+        System.out.println(c=a+b);
+   
+    }
+    
+}
